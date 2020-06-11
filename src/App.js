b7ab0e4c44da0bb34d6d02c00e94dfcc9b1695f6
wangkath <56166245+wangkath@users.noreply.github.com>
2020-06-11 11:19:47 -0700

    Basic Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react"
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+function App() {    
+    return (
+        <div>
+            <Header />
+            <h1>Home Page</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
