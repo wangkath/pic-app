c32f85200c3c9366b0ec3ee504fc104002163fd7
wangkath <56166245+wangkath@users.noreply.github.com>
2020-06-16 11:41:38 -0700

    Icons

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,9 +3,18 @@ import React, {useState} from "react"
 function Image({img, className}) {
     const [hovered,setHovered] = useState(false)
 
+    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i> 
+
     return(
-        <div className = {`${className} image-container`}>
-            <img onMouseEnter = {() => setHovered(true)} onMouseLeave = {() => setHovered(false)} src = {img.url} className = "image-grid"/>
+        <div 
+            className = {`${className} image-container`}
+            onMouseEnter = {() => setHovered(true)} 
+            onMouseLeave = {() => setHovered(false)} 
+        >
+            <img src = {img.url} className = "image-grid"/>
+            {heartIcon}
+            {cartIcon}
         </div>
     )
 }
