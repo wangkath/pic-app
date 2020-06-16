5c1d3d599d3dc5295868a6f9963dcb4444680b0d
wangkath <56166245+wangkath@users.noreply.github.com>
2020-06-16 09:20:23 -0700

    Update State

--- src/Context.js
+++ src/Context.js
@@ -1,11 +1,12 @@
-import React from "react"
+import React, {useState} from "react"
 import { tsPropertySignature } from "@babel/types";
 
 const Context = React.createContext();
 
 function ContextProvidor(props) {
+    const [photoArray,setPhotoArray] = useState([])
     return(
-        <Context.Provider value = "">
+        <Context.Provider value = {photoArray}>
             {props.children}
         </Context.Provider>
     )
