1649320181c8585c33090b245c9d176b72d222f4
wangkath <56166245+wangkath@users.noreply.github.com>
2020-06-22 10:13:07 -0700

    Cart Page

---
+++ src/components/CartItem.js
@@ -0,0 +1,15 @@
+import React, {useContext} from "react"
+import {Context} from "../Context"
+
+function CartItem({item}) {
+    const {removeFromCart} = useContext(Context)
+    return(
+        <div className = "cart-item">
+            <i onClick = {() => removeFromCart(item.id)} className="ri-delete-bin-line"></i>
+            <img src={item.url} width="30%" />
+            <p>$5.99</p>
+        </div>
+    )
+}
+
+export default CartItem
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,15 +1,24 @@
 import React, {useContext} from "react"
 import {Context} from "../Context"
 import Image from "../components/Image"
-import {getClass} from "../utils/index"
+import CartItem from "../components/CartItem"
 
 function Cart() {
     const {cartPhotos} = useContext(Context)
-    const display = cartPhotos.map(item => (<img src = {item.url}></img>))
+    const display = cartPhotos.map(item => (
+        <div>
+            {/* <img src = {item.url}></img> */}
+            <CartItem key = {item.id} item = {item}/>
+        </div>
+    ))
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {display}
+            <p className = "total-cost">Total: </p>
+            <div className="order-button">
+                <button>Place Order</button>
+            </div>
         </main>
     )
 }
