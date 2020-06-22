47ed3077c1f1fea4424a7a587c83c5308c94d5dd
wangkath <56166245+wangkath@users.noreply.github.com>
2020-06-22 10:29:36 -0700

    Add Home Page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import Header from "./components/Header"
 import {Switch, Route} from "react-router-dom"
 import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
+import Home from "./pages/Home"
 
 function App() {    
     return (
         <div>
             <Header />
-            <h1>Home Page</h1>
-
             <Switch>
               <Route exact path = "/">
+                <Home />
                 <Photos />
               </Route>
               <Route path = "/cart">
