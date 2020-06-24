a166ae1ac8704520137194c475e3d300643191b9
wangkath <56166245+wangkath@users.noreply.github.com>
2020-06-24 09:49:16 -0700

    PropTypes

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,6 @@
 import React, {useContext, useState} from "react"
 import {Context} from "../Context"
+import PropTypes from "prop-types"
 
 function CartItem({item}) {
     const {removeFromCart} = useContext(Context)
@@ -19,6 +20,12 @@ function CartItem({item}) {
             <p>$5.99</p>
         </div>
     )
+}
+
+CartItem.propTypes = {
+    item: PropTypes.shape({
+        url: PropTypes.string.isRequired
+    })
 }
 
 export default CartItem
